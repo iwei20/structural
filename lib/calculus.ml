c1163a3f711b6a820117267ea8e7ba798e364cb7
iwei20 <ivanw8288@gmail.com>
2026-04-30 10:56:18 -0400

    feat: Implement extract case

--- lib/calculus.ml
+++ lib/calculus.ml
@@ -15,7 +15,7 @@ module Term = struct
 end
 
 module Dependencies = struct
-    type t = (string * Relation.t * string) list
+    type t = (Term.t * Relation.t * Term.t) list
 end
 
 module FunctionStack = struct
@@ -26,7 +26,18 @@ let rec extract (deps : Dependencies.t) (stack : FunctionStack.t) (term : Term.t
     match term with
     | Var(x) -> AdjacencyList.empty
     | Inj(_, e) -> extract deps stack e
-    | Case(e_scrut, cases) -> AdjacencyList.empty (* TODO *)
+    | Case(e_scrut, cases) ->
+        let calls_scrut = extract deps stack e_scrut in
+        let calls_cases =
+            List.fold_left
+                (fun acc (x, e_case) ->
+                    let added = (Term.Var(x), Relation.Leq, e_scrut) in
+                    AdjacencyList.union
+                        acc
+                        (extract (added::deps) stack e_case))
+                AdjacencyList.empty
+                cases in
+        AdjacencyList.union calls_scrut calls_cases
     | Tuple(components) ->
         List.fold_left
             (fun acc c -> AdjacencyList.union acc (extract deps stack c))
