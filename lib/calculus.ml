12985e7473d526f2b5b9913b70875f680e39eafc
iwei20 <ivanw8288@gmail.com>
2026-04-29 21:21:49 -0400

    feat: Add some boilerplate for extract

--- lib/calculus.ml
+++ lib/calculus.ml
@@ -1,3 +1,5 @@
+include Call
+
 module Term = struct
     type t =
     | Var of string
@@ -5,9 +7,34 @@ module Term = struct
     | Case of t * ((string * t) list)
     | Tuple of t list
     | Proj of int * t
-    | Lam of string * t
+    | Lam of string list * t
     | Rec of string * t
-    | App of t * t
+    | App of t * t list
     | Fold of t
     | Unfold of t
 end
+
+module Dependencies = struct
+    type t = (string * Relation.t * string) list
+end
+
+module FunctionStack = struct
+    type t = (string * string list) list 
+end
+
+let rec extract (deps : Dependencies.t) (stack : FunctionStack.t) (term : Term.t) =
+    match term with
+    | Var(x) -> AdjacencyList.empty
+    | Inj(_, e) -> extract deps stack e
+    | Case(e_scrut, cases) -> AdjacencyList.empty
+    | Tuple(components) ->
+        List.fold_left
+            (fun acc c -> AdjacencyList.union acc (extract deps stack c))
+            AdjacencyList.empty
+            components
+    | Proj(_, e) -> extract deps stack e
+    | Lam(_, e_body) -> extract deps stack e_body
+    | Rec(f, e_body) -> AdjacencyList.empty (* TODO *)
+    | App(e_fun, e_args) -> AdjacencyList.empty (* TODO *)
+    | Fold(e) -> extract deps stack e
+    | Unfold(e) -> extract deps stack e
